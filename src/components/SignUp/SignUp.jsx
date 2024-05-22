543016ca01c1b56fdb525236cb016dc614cbdedb
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-05-22 19:50:07 +0530

    Creating Signup Screen

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,120 @@
+/* eslint-disable react/prop-types */
+import  { useContext, useState } from "react";
+import { AuthContext } from "../../Context/AuthContext";
+
+
+function Signup({ showSignUp, handleToggle }) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ const {login} = useContext(AuthContext)
+ 
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", { username, email, password });
+    try{
+      const response = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCwpa9W1-_fktr3vZIDdvjZZz4iy-3Knro`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username, email, password }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+    const resData = await response.json()
+    login(resData.idToken)
+    console.log(resData);
+    }catch(err){
+    console.log(err)
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-rose-200">
+      <div className="bg-white p-8 rounded shadow-md max-w-md w-full">
+        <h2 className="text-2xl font-semibold mb-4">Signup</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="username"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+              className="w-full px-4 py-2 rounded border focus:outline-none focus:border-blue-500"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              className="w-full px-4 py-2 rounded border focus:outline-none focus:border-blue-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="w-full px-4 py-2 rounded border focus:outline-none focus:border-blue-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            Sign Up
+          </button>
+          {showSignUp && (
+            <button
+              onClick={handleToggle}
+              type="button"
+              className="w-full mt-4 bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Back To LogIn
+            </button>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
