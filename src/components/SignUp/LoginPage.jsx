6e2dc8a3557d58e0af40cd085c2a9b56180e6603
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2024-06-07 22:13:38 +0530

    some changes

--- src/components/SignUp/LoginPage.jsx
+++ src/components/SignUp/LoginPage.jsx
@@ -113,7 +113,7 @@ function LoginPage() {
                     className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                   />
                   <label
-                    htmlFor="remember-me"
+                    
                     className="ml-2 block font-serif text-lg text-gray-900"
                   >
                     Remember me
@@ -121,7 +121,7 @@ function LoginPage() {
                 </div>
 
                 {!OpenPass && (
-                  <div className="text-lg text-black font-serif">
+                  <div className="text-lg text-violet-600 cursor-pointer font-serif">
                     <button onClick={handleOpen}>Forget Password</button>
                   </div>
                 )}
